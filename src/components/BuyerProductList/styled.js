fe69e5b677d0959b255d574527b28b099bf185a2
vchrpng <vchrpng@gmail.com>
2020-01-19 13:35:56 +0700

    improve product list style

--- src/components/BuyerProductList/styled.js
+++ src/components/BuyerProductList/styled.js
@@ -3,6 +3,10 @@ import styled from 'styled-components'
 export const Thumbnail = styled.div`
     text-align:center;
     font-size:12px;
-    height:350px;
+    height:300px;
+    background:#f2f2f2;
+    border-radius: 10px;
+    overflow: hidden;
+    box-shadow: 6px 6px 5px -8px;
 `
 
