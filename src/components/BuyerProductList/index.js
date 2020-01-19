14dd0e3b5511093fccb94b98d7b71c17d7d4c5eb
vchrpng <vchrpng@gmail.com>
2020-01-19 13:36:27 +0700

    using modal to view project detail

--- src/components/BuyerProductList/index.js
+++ src/components/BuyerProductList/index.js
@@ -1,34 +1,33 @@
 import React from 'react'
-import { Image } from 'semantic-ui-react'
+import Product from '../../containers/Product'
+import { Image, Modal } from 'semantic-ui-react'
 import { Thumbnail } from './styled'
 import { Row , Column } from '../../theme/Grid'
-import { Link } from 'react-router-dom'
 
 
 const BuyerProductList = ({ product }) => {
 
 
     return(
-      
-        <Thumbnail>
-            <Link to={`/products/${product.category}/${product.id}`}>
-            <Image src={product.thumbnail}/>
-            </Link>
-            <Row>
-                <Column style={{marginTop:'10px'}}>
-                    <strong>   
-                    {product.title} 
-                    <br/>
-                    ({product.color})
-                    </strong>
-                </Column>
-                <Column>
-                    {product.price} $
-                </Column>
-            </Row>
-        
-        </Thumbnail>
-      
+        <Modal trigger={
+            <Thumbnail>
+                <Image src={product.thumbnail}/>
+                <Row>
+                    <Column style={{marginTop:'10px'}}>
+                        <strong>   
+                        {product.title} 
+                        <br/>
+                        ({product.color})
+                        </strong>
+                    </Column>
+                    <Column>
+                        {product.price} $
+                    </Column>
+                </Row>
+            </Thumbnail>
+        }>
+        <Product id={product.id} />
+        </Modal>
     )
 }
 
