30cbd337254ad089e6d2b5c73ee804cea779d532
vchrpng <vchrpng@gmail.com>
2020-01-19 16:29:06 +0700

    remove background for product item

--- src/components/BuyerProductList/styled.js
+++ src/components/BuyerProductList/styled.js
@@ -4,7 +4,6 @@ export const Thumbnail = styled.div`
     text-align:center;
     font-size:12px;
     height:300px;
-    background:#f2f2f2;
     border-radius: 10px;
     overflow: hidden;
     box-shadow: 6px 6px 5px -8px;
