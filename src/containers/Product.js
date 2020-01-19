3d913f28b11e6fc17718d81fa32f1eabee15364b
vchrpng <vchrpng@gmail.com>
2020-01-19 13:35:25 +0700

    break the page into small component

--- src/containers/Product.js
+++ src/containers/Product.js
@@ -3,16 +3,12 @@ import { connect } from 'react-redux'
 import { Grid , Container , Image } from 'semantic-ui-react'
 import ShippingDetail from '../components/ShippingDetail'
 import ProductDescription from '../components/ProductDescription'
-import Nav from '../components/Nav'
-import CategoryList from '../components/CategoryList'
-import Footer from '../components/Footer'
 import Slider from 'react-slick'
 import { showProducts } from '../selectors'
 
 
-const Product = ({ match , products , addToBag , inventory }) => {
-
-    const { id } = match.params
+const Product = ({ id , products , addToBag , inventory }) => {
+    
     const currentProduct = products.filter(product => {
         return product.id === parseInt(id,10)
     })
@@ -31,76 +27,10 @@ const Product = ({ match , products , addToBag , inventory }) => {
                 columns={5} 
                 style={{textAlign:'center'}}
             >
-                <Grid.Column computer={1}/>
-                <Grid.Column
-                    only={'computer'}
-                    computer={3} 
-                    style={{top:'30%',position:'sticky',bottom:'20%',left:'0'}}>
-                        <ProductDescription 
-                            currentProduct={current}
-                        />
-                </Grid.Column>
-                <Grid.Column
-                    only={'computer'}
-                    style={{margin:'0 auto'}}
-                    computer={4}
-                >
-                    <Container>
-                        {current.images.map((pic,index) =>
-                            <Image src={pic} key={index} style={{marginBottom:'50px'}}/>
-                        )}
-                    </Container>
-                </Grid.Column>
-                <Grid.Column  
-                    only='computer'
-                    computer={3} 
-                    style={{top:'30%',position:'sticky',bottom:'27%',right:'0'}}>
-                    <Container>
-                        <ShippingDetail 
-                            currentProduct={current}
-                            inventory={inventory}
-                        >
-                            {current.price} $ USD
-                        </ShippingDetail>   
-                    </Container>
-                </Grid.Column>
-                <Grid.Column
-                    only={'tablet'}
-                    style={{margin:'0 auto'}}
-                    tablet='6'
-                >
-                    <Container>
-                    <Slider {...settings}>
-                        {current.images.map((pic,index) =>
-                            <Image size='mini' src={pic} key={index} style={{marginBottom:'50px'}}/>
-                        )}
-                    </Slider>
-                    </Container>
-                </Grid.Column>
                 <Grid.Column
                     style={{margin:'0 auto'}}
-                    only='tablet'
-                    tablet='6'
+                    width='6'
                 >
-                    <ProductDescription 
-                            currentProduct={current}
-                        />
-                    <Container>
-                        <ShippingDetail 
-                            currentProduct={current}
-                            inventory={inventory}
-                        >
-                            {current.price} $ USD
-                        </ShippingDetail>   
-                    </Container>
-                </Grid.Column>
-                
-                
-                <Grid.Column
-                only={'mobile'}
-                style={{margin:'0 auto'}}
-                textAlign='center'
-                mobile='14'>
                     <Container>
                     <Slider {...settings}>
                         {current.images.map((pic,index) =>
@@ -112,9 +42,10 @@ const Product = ({ match , products , addToBag , inventory }) => {
 
                 <Grid.Column
                     style={{margin:'50px auto'}}
-                    only='mobile'
-                    mobile='16'
+                    width='6'
                 >
+                    <ProductDescription currentProduct={current} />
+
                     <Container>
                         <ShippingDetail 
                             currentProduct={current}
@@ -124,23 +55,18 @@ const Product = ({ match , products , addToBag , inventory }) => {
                         </ShippingDetail>   
                     </Container>
                 </Grid.Column>
-                <Grid.Column computer={1}/>
-
             </Grid.Row>)
 
                 
 
     return(
             <div>
-                <Nav/>
-                <CategoryList/>
                 <Grid  
                     style={{padding:'40px'}} 
                     verticalAlign='middle'
                 >
                     {test}
                 </Grid>
-                <Footer/>
             </div>
     )
 }
