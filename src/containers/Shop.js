9878de3e0134585f2e739eb3823bf3d76bd319db
vchrpng <vchrpng@gmail.com>
2020-05-17 19:36:17 +0700

    style(Container): add navigation

--- src/containers/Shop.js
+++ src/containers/Shop.js
@@ -4,25 +4,29 @@ import { DisplayGrid } from '../theme/Grid'
 import { connect } from 'react-redux'
 import { showProducts } from '../selectors'
 import BuyerProductList from '../components/BuyerProductList'
+import Nav from '../components/Nav'
 
 
 const Shop = ({ products }) => {
     return (
-                <Grid>
-                    <Grid.Row centered>
-                    <Grid.Column computer={13} tablet={16}>
-                    <div style={{marginTop:'50px'}}>
-                        <DisplayGrid column={'repeat(auto-fill,17em)'} colGap={50} rowGap={70}>
-                            {products.map((product,index) => 
-                        <BuyerProductList 
-                            key={index}
-                            product={product}/>
-                        )}
-                       </DisplayGrid>
-                    </div>
-                    </Grid.Column>
-                    </Grid.Row>
-                </Grid>
+        <div>
+            <Nav />
+            <Grid>
+                <Grid.Row centered>
+                <Grid.Column computer={13} tablet={16}>
+                <div style={{marginTop:'50px'}}>
+                    <DisplayGrid column={'repeat(auto-fill,17em)'} colGap={50} rowGap={70}>
+                        {products.map((product,index) => 
+                    <BuyerProductList 
+                        key={index}
+                        product={product}/>
+                    )}
+                    </DisplayGrid>
+                </div>
+                </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
         )
     }
 
