308cc8e9f4175817aca464ee50ab0e783a669791
vchrpng <vchrpng@gmail.com>
2020-05-22 16:18:34 +0700

    feat(Checkout): using validation

---
+++ src/components/CheckoutForm/validate.js
@@ -0,0 +1,22 @@
+export default validate = values => {
+    const errors = {};
+    if (!values.fullname) {
+      errors.fullname = 'Required';
+    } else if (values.fullname.length > 40) {
+      errors.fullname = 'Must be 40 characters or less';
+    }
+  
+    if (!values.lastName) {
+      errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+      errors.lastName = 'Must be 20 characters or less';
+    }
+  
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+  
+    return errors;
+  };
