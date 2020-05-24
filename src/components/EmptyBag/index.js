88a11aff895c7d7fa03c61d59b7cb766d1fde2ce
vchrpng <vchrpng@gmail.com>
2020-05-24 13:09:08 +0700

    style(EmptyBag): remove some elements

--- src/components/EmptyBag/index.js
+++ src/components/EmptyBag/index.js
@@ -1,31 +1,12 @@
 import React from 'react'
-import { DisplayGrid , Flex } from '../../theme/Grid'
-import { ButtonStyled , CustomLink } from '../Etc/Reusable'
 
 
 const EmptyBag = () => {
     return (
-        <div style={{
-            minWidth: '300px'
-        }}>
-            <DisplayGrid 
-                style={{marginTop:'100px'}}  
-                autoRows={'70px,auto'}
-                >
-                <Flex justify={'center'} align={'center'}>
-                    <h4 style={{fontWeight:'100',letterSpacing:'1px'}}>
-                        Your bag is empty please go to shopping.
-                    </h4></Flex>
-                <Flex justify={'center'} align={'center'}>
-                <CustomLink style={{width:'65%'}} to='/'>  
-                    <ButtonStyled>
-                        
-                            GO SHOPPING
-                    
-                    </ButtonStyled>
-                    </CustomLink>
-                </Flex>
-            </DisplayGrid>
+        <div>
+            <h4 style={{fontWeight:'100',letterSpacing:'1px'}}>
+                Your bag is empty please go to shopping.
+            </h4>
         </div>
     )
 }
