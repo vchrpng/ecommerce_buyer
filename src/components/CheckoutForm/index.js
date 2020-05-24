78f841ef0b7a8174238c969e10660e89e219e748
vchrpng <vchrpng@gmail.com>
2020-05-24 10:39:16 +0700

    refactor(Checkout): refactor

--- src/components/CheckoutForm/index.js
+++ src/components/CheckoutForm/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { InputText } from '../Etc/Checkout'
 import OrderSummary from '../OrderSummary'
 import { RedButton } from '../Etc/Reusable'
 import CheckoutNavigate from '../CheckoutNavigate'
 import validate from './validate'
 import { connect } from 'react-redux'
-import { Formik, Field, Form, ErrorMessage } from 'formik'
+import { Formik, Form, ErrorMessage } from 'formik'
 import { submitOrder } from '../../actions'
 import { totalSelector, selectedProducts , selectedSizes } from '../../selectors'
 import {
@@ -71,46 +70,36 @@ class CheckoutForm extends React.Component {
                     <div style={{marginTop:'20px'}}>
                         <Form onSubmit={this.onSubmit} loading={loading}>
                             <h3>Customer Information</h3>
-                            <Field  name={'fullname'}
+                            <InputText  name={'fullname'}
                                 placeholder={'Full name'}
-                                type={'text'}>
-             
-                            </Field>
-                            <Field name={'phone'}
+                                type={'text'}
+                            />
+                            <InputText name={'phone'}
                                 placeholder={'Phone'}
-                                type={'text'}>
-                           
-                            </Field>
+                                type={'text'}
+                            />
                             <h3 style={{marginTop:'0'}}>Shipping Address</h3>
                             <div>
-                                <Field name={'city'}
+                                <InputText name={'city'}
                                     placeholder={'City'}
-                                    type={'text'} >
-                                {/* <InputText
-                                    
-                                    onChange={this.onChange}
-                                    
-                            /> */}
-                            </Field>
-                            <Field
-                              name={'province'}
-                              placeholder={'Province'}
-                              type={'text'}>
-                               
-                                </Field>
+                                    type={'text'} 
+                                />
+                                <InputText
+                                    name={'province'}
+                                    placeholder={'Province'}
+                                    type={'text'}
+                                />
                             </div>
                             <div>
-                            <Field  name={'postcode'}
-                                    placeholder={'Postcode'}
-                                    type={'text'}>
-                               
-                            </Field>
-                            <Field 
-                             name={'country'}
-                             placeholder={'Country'}
-                             type={'text'}>
-                           
-                            </Field>
+                                <InputText  name={'postcode'}
+                                        placeholder={'Postcode'}
+                                        type={'text'}
+                                />
+                                <InputText 
+                                    name={'country'}
+                                    placeholder={'Country'}
+                                    type={'text'}
+                                />
                             </div>
                             <div>
                             <label>
