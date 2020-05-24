6bb84925bc524e39daf4ed42acee51d47b46d7cd
vchrpng <vchrpng@gmail.com>
2020-05-24 10:55:21 +0700

    style(ProductList): resize product thumbnail

--- src/components/BuyerProductList/styled.js
+++ src/components/BuyerProductList/styled.js
@@ -3,10 +3,9 @@ import styled from 'styled-components'
 export const Thumbnail = styled.div`
     text-align:center;
     font-size:12px;
-    height:290px;
+    height:260px;
     border-radius: 10px;
     overflow: hidden;
-    box-shadow: 6px 6px 5px -8px;
     cursor:pointer;
 `
 
