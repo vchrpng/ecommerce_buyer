1a1247f1603ab443680f6aa4d825bd1ec756e774
vchrpng <vchrpng@gmail.com>
2020-05-25 17:00:59 +0700

    style(CheckOut): use styles

---
+++ src/components/CheckoutForm/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const OrderFormLayout = styled.div`
+    margin-top:20px;
+    
+    .input-group {
+        display: flex;
+    }
+    
+    .payment-section {
+        background: red;
+    }
+
+
+`
