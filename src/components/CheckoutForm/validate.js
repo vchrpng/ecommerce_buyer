8cdc7fb998ee412e739cbe0d34ca35e134795cba
vchrpng <vchrpng@gmail.com>
2020-05-31 14:26:50 +0700

    feat(Validate): improve payment form validation

--- src/components/CheckoutForm/validate.js
+++ src/components/CheckoutForm/validate.js
@@ -1,22 +1,24 @@
-// export default values => {
-//     const errors = {};
-//     if (!values.fullname) {
-//       errors.fullname = 'Required';
-//     } else if (values.fullname.length > 40) {
-//       errors.fullname = 'Must be 40 characters or less';
-//     }
-  
-//     if (!values.lastName) {
-//       errors.lastName = 'Required';
-//     } else if (values.lastName.length > 20) {
-//       errors.lastName = 'Must be 20 characters or less';
-//     }
-  
-//     if (!values.email) {
-//       errors.email = 'Required';
-//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-//       errors.email = 'Invalid email address';
-//     }
-  
-//     return alert(JSON.stringify(errors),null,2);
-//   };
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+export default values => {
+    return sleep(2000).then(() => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = 'Required';
+        } else if (values.name.length > 40) {
+          errors.name = 'Must be 40 characters or less';
+        }
+      
+        if (!values.lastName) {
+          errors.lastName = 'Required';
+        } else if (values.lastName.length > 20) {
+          errors.lastName = 'Must be 20 characters or less';
+        }
+      
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+          errors.email = 'Invalid email address';
+        }
+    })
+  };
