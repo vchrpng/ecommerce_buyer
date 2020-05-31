ed405fee7a34a4daac808b7af7136ef904c366db
vchrpng <vchrpng@gmail.com>
2020-05-31 16:47:26 +0700

    wip(Checkout): change checkout form style

--- src/components/CheckoutForm/index.js
+++ src/components/CheckoutForm/index.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { InputText } from '../Etc/Checkout'
 import OrderSummary from '../OrderSummary'
 import { ProceedPayment } from '../Etc/Reusable'
 import CheckoutNavigate from '../CheckoutNavigate'
 import validationSchema from './validate'
 import { connect } from 'react-redux'
 import { Formik, Form, 
-    // ErrorMessage
+    ErrorMessage
  } from 'formik'
 import { submitOrder } from '../../actions'
 import { OrderFormLayout } from './styled'
 import { totalSelector, selectedProducts , selectedSizes } from '../../selectors'
 import { CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import ShippingAddressForm from '../ShippingAddressForm'
+import Modal from '../Modal'
 
 const lockIcon = require('../../assets/lock.svg') 
 
@@ -26,6 +27,8 @@ const initialValues = {
 const CheckoutForm = ({ products, total, size, order, submit }) => {
     const elements = useElements();
     const stripe = useStripe();
+
+    const [isDeliveryAddressOpen, toggleDeliveryAddressForm] = React.useState(false)
 
 
 
@@ -58,80 +61,50 @@ const CheckoutForm = ({ products, total, size, order, submit }) => {
         >
             {({ isSubmitting, handleSubmit, errors, touched }) => (
                 <OrderFormLayout>
-                    <Form onSubmit={handleSubmit}>
-                        <div className="checkout-box">
-                            <aside className="shipping-destination">
-                                <h3>Customer Information</h3>
-                                {errors.name && touched.name ? (
-                                    <div>{errors.name}</div>
-                                ) : null}
-                                <InputText name={'name'}
-                                    placeholder={'Full name'}
-                                    type={'text'}
-                                />
-                                <InputText name={'phone'}
-                                    placeholder={'Phone'}
-                                    type={'text'}
-                                />
-                                <h3 style={{marginTop:'0'}}>Shipping Address</h3>
-                                <div className="input-group">
-                                    <InputText name={'city'}
-                                        placeholder={'City'}
-                                        type={'text'} 
-                                    />
-                                    <InputText
-                                        name={'province'}
-                                        placeholder={'Province'}
-                                        type={'text'}
-                                    />
-                                </div>
-                                <div className="input-group">
-                                    <InputText  name={'line1'}
-                                            placeholder={'Address Line'}
-                                            type={'text'}
-                                    />
-                                    <InputText 
-                                        name={'country'}
-                                        placeholder={'Country'}
-                                        type={'text'}
-                                    />
-                                </div>
-                            </aside>
-                            <aside className="payment-method">
-                                <div className="invoice-detail">
-                                    <div className="credit-card">
-                                        <CardElement options={{
-                                            style: {
-                                                base: { fontSize: '16px', color: '#424770',
-                                                '::placeholder': {color: '#aab7c4' },
-                                                },
-                                                invalid: { color: '#9e2146' },
-                                            }}}
-                                        />
-                                    </div>
-                                    <div >
-                                        <OrderSummary 
-                                            products={products}
-                                            total={total}
-                                            size={size}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="pay-button">
-                                    <ProceedPayment type="submit" disabled={isSubmitting}>
-                                        <img className="secure-icon" src={lockIcon} />
-                                        <h3>{isSubmitting ? 'LOADING' : `Pay ${total} $`}</h3>
-                                    </ProceedPayment>
-                                </div>
-                            </aside>
-                        </div>
+                         <button onClick={() => toggleDeliveryAddressForm(true)}></button>
+
+                    <OrderSummary 
+                        products={products}
+                        total={total}
+                        size={size}
+                        />
+                    {isDeliveryAddressOpen && 
+                        <Modal
+                            id="modal"
+                            isOpen={isDeliveryAddressOpen}
+                            onClose={toggleDeliveryAddressForm}
+                            header="Delivery Address"
+                        >
+                            <ShippingAddressForm  />
+                        </Modal>}
+                        <aside className="payment-method">
+                         <div className="invoice-detail">
+                             <div className="credit-card">
+                                 <CardElement options={{
+                                     style: {
+                                         base: { fontSize: '16px', color: '#424770',
+                                         '::placeholder': {color: '#aab7c4' },
+                                         },
+                                         invalid: { color: '#9e2146' },
+                                     }}}
+                                 />
+                             </div>
+                             <div >
+
+                             </div>
+                         </div>
+                         <div className="pay-button">
+                             <ProceedPayment type="submit" disabled={isSubmitting}>
+                                 <img className="secure-icon" src={lockIcon} />
+                                 <h3>{isSubmitting ? 'LOADING' : `Pay ${total} $`}</h3>
+                             </ProceedPayment>
+                         </div>
+                     </aside>
+                                      
+                    <div className="confirm-payment">
+                        <CheckoutNavigate />
                         
-                        
-                        <div className="confirm-payment">
-                            <CheckoutNavigate />
-                            
-                        </div>
-                    </Form>
+                    </div>
                 </OrderFormLayout>
             )}
         </Formik>
