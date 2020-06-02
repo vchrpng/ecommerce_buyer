77ec52fa272d80aafda42487f52a6d6deb174ed7
vchrpng <vchrpng@gmail.com>
2020-06-02 19:29:19 +0700

    feat(CurrentProduct): add a description

--- src/components/ProductDescription/index.js
+++ src/components/ProductDescription/index.js
@@ -1,14 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const ProductDetailContainer = styled.div`  
+    .product-title {
+        font-size: 18px;
+    }
+    .product-fabric {
+        color: gray;
+    }
+    .product-price {
+        margin: 10px 0;
+    }
+    .product-description {
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+`
 
 const ProductDescription = ({ currentProduct }) => {
     return (
-        <div>
-            <p style={{fontSize:'18px'}}>{currentProduct.title}</p>         
-            <p>100% Nylon imported</p>
-            <div>
+        <ProductDetailContainer>
+            <p className="product-title">{currentProduct.title}</p>         
+            <p className="product-fabric">100% Nylon imported</p>
+            <div className="product-price">
                 <h3>{currentProduct.price} $ USD</h3>
             </div>
-        </div>
+            <div className="product-description">
+                <p>{currentProduct.description}</p>
+            </div>
+        </ProductDetailContainer>
     )
 }
 
