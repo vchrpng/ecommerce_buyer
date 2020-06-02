8a345f37833305f83a8597856f79bb50e08fd0f8
vchrpng <vchrpng@gmail.com>
2020-06-02 19:04:22 +0700

    fix(ProductDescription): try to not use semantic

--- src/components/ProductDescription/index.js
+++ src/components/ProductDescription/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Container} from 'semantic-ui-react'
 
 const ProductDescription = ({ currentProduct }) => {
     return (
-        <Container>
+        <div>
             <p style={{fontSize:'18px'}}>{currentProduct.title}</p>         
             <p>100% Nylon imported</p>
             <div>
                 <h3>{currentProduct.price} $ USD</h3>
             </div>
-        </Container>
+        </div>
     )
 }
 
