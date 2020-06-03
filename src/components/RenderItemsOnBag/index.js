e14ba1c8440d1aa5d78fdc60c5ee863bdbe1ba76
vchrpng <vchrpng@gmail.com>
2020-06-03 12:13:36 +0700

    fix(ItemsOnBag): using default element

--- src/components/RenderItemsOnBag/index.js
+++ src/components/RenderItemsOnBag/index.js
@@ -1,36 +1,32 @@
 import React from 'react'
-import { Table , Image } from 'semantic-ui-react'
 import { Label } from '../Etc/Label'
 import { ButtonStyled } from '../Etc/Reusable'
 import { connect } from 'react-redux'
-import { ProdImg } from '../Responsive'
 import { deleteFromBag } from '../../actions'
 
 const RenderItemsOnBag = ({ product , deleteFromBag , size }) => {
     return (
         product.map((item,idx) =>
-            <Table.Body key={idx}>
-                <Table.Row>
-                    <ProdImg width={2}>
-                        <Image style={{marginLeft:'20%'}} size="tiny" src={item.thumbnail}/>
-                    </ProdImg>
-                    <Table.Cell width={10} verticalAlign={'top'} style={{borderLeft:'none'}}>
+            <tr key={idx}>
+                    <td >
+                        <img style={{ width: '80px' }} src={item.thumbnail}/>
+                    </td>
+                    <td >
                         <Label weight={'bold'}>{item.title}</Label>
                         <Label weight={'lighter'}>{item.category}</Label>
                         <Label weight={'lighter'}>Size : {size[idx]}</Label>
-                    </Table.Cell>
-                    <Table.Cell width={2} style={{borderLeft:'none'}}>
+                    </td>
+                    <td  >
                         <ButtonStyled onClick={() => 
                             deleteFromBag(idx,size[idx],item.id)}
                         >  REMOVE </ButtonStyled>
-                    </Table.Cell>
-                    <Table.Cell width={2} 
+                    </td>
+                    <td  
                         textAlign={'right'} 
-                        style={{borderLeft:'none'}}>
+                        >
                         <label>${item.price.toFixed(2)}</label>    
-                    </Table.Cell>
-                    </Table.Row>
-            </Table.Body>
+                    </td>
+            </tr>
         )
     )
 }
