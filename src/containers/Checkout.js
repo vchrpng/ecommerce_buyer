c0d5fc7d9b4ab3732bb750af2dd7cf78101c6c17
vchrpng <vchrpng@gmail.com>
2020-06-05 21:48:13 +0700

    style(Checkout): add shadow

--- src/containers/Checkout.js
+++ src/containers/Checkout.js
@@ -16,6 +16,9 @@ const CheckOutPageContainer = styled.div`
     max-width: 800px;
     display: flex;
     justify-content: center;
+    margin-top: 30px;
+    border-radius:20px;
+    box-shadow: 0px 0px 26px -8px rgba(0,0,0,0.75);
 
     @media only screen and (max-width : 750px){
             flex-direction: column-reverse;
