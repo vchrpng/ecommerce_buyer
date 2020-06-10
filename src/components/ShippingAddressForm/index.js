c51d3d517e75abab41e9bd9dbc5368faf3e18fa3
vchrpng <vchrpng@gmail.com>
2020-06-10 20:03:32 +0700

    style(ShippingAddress): change position elements

--- src/components/ShippingAddressForm/index.js
+++ src/components/ShippingAddressForm/index.js
@@ -10,41 +10,38 @@ const ShippingAddressForm = () => {
             <form>
                  <div className="checkout-box">
                      <section className="shipping-destination">
-                         <h3>New Delivery Address</h3>
-                         <div className="input-group">
-                            <h4>Customer Information</h4>
-                                <InputText name={'name'}
-                                    placeholder={'Full name'}
+                        <h3>New Delivery Address</h3>
+                        <InputText name={'name'}
+                            placeholder={'Full name'}
+                            type={'text'}
+                        />
+                        <InputText name={'phone'}
+                            placeholder={'Phone'}
+                            type={'text'}
+                        />
+                        <div className="input-group">
+                            <InputText name={'city'}
+                                placeholder={'City'}
+                                type={'text'} 
+                            />
+                            <InputText
+                                name={'province'}
+                                placeholder={'Province'}
+                                type={'text'}
+                            />
+                        </div>
+                        <div className="input-group">
+                            <InputText  name={'line1'}
+                                    placeholder={'Address Line'}
                                     type={'text'}
-                                />
-                                <InputText name={'phone'}
-                                    placeholder={'Phone'}
-                                    type={'text'}
-                                />
-                         </div>
-                         <div className="input-group">
-                             <InputText name={'city'}
-                                 placeholder={'City'}
-                                 type={'text'} 
-                             />
-                             <InputText
-                                 name={'province'}
-                                 placeholder={'Province'}
-                                 type={'text'}
-                             />
-                         </div>
-                         <div className="input-group">
-                             <InputText  name={'line1'}
-                                     placeholder={'Address Line'}
-                                     type={'text'}
-                             />
-                             {/* <InputText 
-                                 name={'country'}
-                                 placeholder={'Country'}
-                                 type={'text'}
-                             /> */}
-                             <CountryList setCountry={setCountry}/>
-                         </div>
+                            />
+                            {/* <InputText 
+                                name={'country'}
+                                placeholder={'Country'}
+                                type={'text'}
+                            /> */}
+                            <CountryList setCountry={setCountry}/>
+                        </div>
                      </section>
                      <section>
                          <ProceedPayment>Submit</ProceedPayment>
