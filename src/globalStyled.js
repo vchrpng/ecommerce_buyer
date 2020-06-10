1dda0bb6842da7793deaa3cff7f0bf0ab8775a98
vchrpng <vchrpng@gmail.com>
2020-06-10 21:38:10 +0700

    style(*): change ul element global scrollbar

--- src/globalStyled.js
+++ src/globalStyled.js
@@ -5,11 +5,19 @@ body ::-webkit-scrollbar-track {
   background: rgb(0,0,0,0); 
 }
 
+
+
+
 body {
+  
   ul {
+
     padding:0;
     margin:0;
     list-style:none;
   }
+  ul::-webkit-scrollbar {
+      height: 7px; 
+    }
 }
 `
