4aeca56b33b5381c162fee2b2da0ac7b77bdff76
vchrpng <vchrpng@gmail.com>
2020-06-15 11:13:11 +0700

    feat(ShippingAddress): update validation message

--- src/components/ShippingAddressForm/validate.js
+++ src/components/ShippingAddressForm/validate.js
@@ -2,23 +2,23 @@ import * as Yup from 'yup'
 
 const schema = Yup.object().shape({
     name: Yup.string()
-        .min(2, 'Too Short!')
-        .max(70, 'Too Long!')
+        .min(2, 'Name is too short!')
+        .max(50, 'Name is too long!')
         .required('Required'),
     phone: Yup.string()
-        .length(9, 'Invalid phone!')
+        .length(9, 'Invalid phone number!')
         .required('Required'),
     city: Yup.string()
-        .min(2, 'Too Short!')
-        .max(70, 'Too Long!')
+        .min(2, 'City is too short!')
+        .max(50, 'City is too long!')
         .required('Required'),
     province: Yup.string()
-        .min(2, 'Too Short!')
-        .max(70, 'Too Long!')
+        .min(2, 'Province is too short!')
+        .max(50, 'Province is too long!')
         .required('Required'),
     line1: Yup.string()
-        .min(2, 'Too Short!')
-        .max(70, 'Too Long!')
+        .min(2, 'Address line is too short!')
+        .max(150, 'Address line is too long!')
         .required('Required'),
 })
 
