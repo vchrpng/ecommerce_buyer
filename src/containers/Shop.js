6d2a920b69d2d77bd41ebcd8967442bdb8b85f85
Fjoepotibutr <vchrpng@gmail.com>
2020-11-11 19:16:34 +0700

    add new loader

--- src/containers/Shop.js
+++ src/containers/Shop.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import { showProducts } from '../selectors'
 import BuyerProductList from '../components/BuyerProductList'
 import Nav from '../components/Nav'
+import BeatLoader from "react-spinners/BeatLoader";
 
 
 const CustomerShopContainer = styled.main`
@@ -22,13 +23,20 @@ const Shop = ({ products }) => {
                 <Grid.Row centered>
                 <Grid.Column computer={13} tablet={16}>
                 <div style={{marginTop:'20px'}}>
+                    {products.length ? 
                     <DisplayGrid column={'repeat(auto-fill,12em)'} colGap={25}>
-                        {products.map((product,index) => 
-                        <BuyerProductList 
-                            key={index}
-                            product={product}/>
+                    {products.map((product,index) => 
+                    <BuyerProductList 
+                        key={index}
+                        product={product}/>
                         )}
-                    </DisplayGrid>
+                    </DisplayGrid> :
+                    <div style={{ width: '100%', height: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}> 
+                        <BeatLoader
+                            size={20}
+                            color={"#333333"}
+                        /> 
+                    </div>}
                 </div>
                 </Grid.Column>
                 </Grid.Row>
